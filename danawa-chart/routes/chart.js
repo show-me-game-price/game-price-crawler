4c9d959b33a9d3e3e8610c8bf219b85257d96d64
jw.seong <jw.seong@sk.com>
2020-02-25 12:56:08 +0900

    d3 chart added

--- danawa-chart/routes/chart.js
+++ danawa-chart/routes/chart.js
@@ -4,6 +4,7 @@ var fs = require('fs')
 var await = require('await')
 
 var danawa = require('../sql')
+var danawa_d3 = require('../sql_d3')
 /* GET chart. */
 router.get('/', function(req, res, next) {
     // res.send('respond with a resource');
@@ -28,9 +29,24 @@ router.get('/rank/raw', async function(req, res, next) {
     // danawa().then(result => res.send(result));
 });
 
-router.get('/d3', function(req, res, next) {
+router.get('/d3/PS4/rank/raw', async function(req, res, next) {
+    const result = await danawa_d3("PS4","rank");
+    res.send(result);
+});
+
+router.get('/d3/PS4/price/raw', async function(req, res, next) {
+    const result = await danawa_d3("PS4", "price");
+    res.send(result);
+});
+
+router.get('/d3/PS4/rank', function(req, res, next) {
     // res.send('respond with a resource');
     // res.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력
-    fs.createReadStream("./views/d3.html").pipe(res); // 같은 디렉토리에 있는 index.html를 response 함
+    fs.createReadStream("./views/ps4rank.html").pipe(res); // 같은 디렉토리에 있는 index.html를 response 함
+});
+router.get('/d3/PS4/price', function(req, res, next) {
+    // res.send('respond with a resource');
+    // res.writeHead(200,{"Content-Type":"text/html"}); // 웹페이지 출력
+    fs.createReadStream("./views/ps4price.html").pipe(res); // 같은 디렉토리에 있는 index.html를 response 함
 });
 module.exports = router;
